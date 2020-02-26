e6f4448341754a39a280dd1887fdaf7a00c8e8f4
Laxmikant Verma <laxmikant91111@gmail.com>
2020-02-26 13:52:50 +0530

    Add files via upload

---
+++ application/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+
+$autoload['libraries'] = array('database','upload');
+
+
+$autoload['drivers'] = array();
+
+
+$autoload['helper'] = array('url','html','form');
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
+
+$autoload['model'] = array('adminm');
---
+++ application/config/routes.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$route['default_controller'] = 'userc';
+$route['about'] = 'userc/about';
+$route['contact'] = 'userc/contact';
+$route['car_ac'] = 'userc/car_ac';
+$route['car_detail'] = 'userc/car_detail';
+$route['book_car'] = 'userc/book_car';
+$route['404_override'] = 'userc/error';
+$route['translate_uri_dashes'] = FALSE;
